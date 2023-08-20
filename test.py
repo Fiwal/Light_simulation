c4401fe1341a5a0823e3b320a1c273e1dd5b67f5
Fiwal <fiwalczowski@gmail.com>
2023-08-20 20:27:05 +0200

    new version

--- test.py
+++
@@ -1,8 +0,0 @@
-# left, middle, right = pygame.mouse.get_pressed()
-#
-# if left:
-#
-#     mouse_x, mouse_y = pygame.mouse.get_pos()
-#     rel_x, rel_y = mouse_x - self.x, mouse_y - self.y
-#
-#     self.angle = math.atan2(rel_y, rel_x)
